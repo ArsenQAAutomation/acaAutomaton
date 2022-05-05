ce5c6d85d5ddcd3ee87f29a8bb9eefd88080fdf7
Ruzan Mahari <ruzan.mahari@iunetworks.local>
2022-05-05 20:45:44 +0400

    added text file

--- src/test/java/ruzan_mahari/PageHeatherTest.java
+++ src/test/java/ruzan_mahari/PageHeatherTest.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
