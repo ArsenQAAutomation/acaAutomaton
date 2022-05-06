3e91985c5e45bc90cb9af51d4f28eacf04cd9a1c
GorBabloyan <104680741+GorBabloyan@users.noreply.github.com>
2022-05-06 09:45:11 +0400

    TestTitle_2

--- src/test/java/GorBabloyan/TitleTest.java
+++ src/test/java/GorBabloyan/TitleTest.java
@@ -1,28 +1,39 @@
 package GorBabloyan;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TitleTest {
     public WebDriver driver;
+
     @BeforeClass
-    void setup(){
+    void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
+
     @Test
-    void Title(){
+    void Title() {
         driver.get("http://automationpractice.com/index.php"); //
         String title = driver.getTitle();
-        Assert.assertEquals(title,"My Store");
+        Assert.assertEquals(title, "My Store");
     }
+
+    @Test
+    void Title_2() {
+        driver.get("http://automationpractice.com/index.php");
+        String title = driver.findElement(By.xpath("//*[@id=\"header_logo\"]/a")).getAttribute("title");
+        Assert.assertEquals(title, "My Store");
+    }
+
     @AfterClass
-    void quit(){
+    void quit() {
         driver.quit();
     }
 }
