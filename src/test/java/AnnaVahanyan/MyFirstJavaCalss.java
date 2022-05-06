ce6e0d4c5a50a970e963c7c32070b52a330acc58
Anna234457 <104854228+Anna234457@users.noreply.github.com>
2022-05-07 00:10:50 +0400

    ed

--- src/test/java/AnnaVahanyan/MyFirstJavaCalss.java
+++ src/test/java/AnnaVahanyan/MyFirstJavaCalss.java
@@ -1,4 +1,23 @@
 package AnnaVahanyan;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyFirstJavaCalss {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        try {
+
+            driver.get("http://automationpractice.com/index.php");
+            WebElement element = driver.findElement(By.tagName(" title "));
+            System.out.println("The title name is :" + element.getAttribute("text"));
+            assertEquals(" My store ", element.getAttribute("text"));
+        } finally {
+            driver.quit();
+        }
+    }
 }
+
