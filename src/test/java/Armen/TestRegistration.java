679a04ee6bc3d86441cf19d181d44cf82de61c6f
GorBabloyan <104680741+GorBabloyan@users.noreply.github.com>
2022-05-18 00:09:14 +0400

    Selenium_Registration

--- src/test/java/Armen/TestRegistration.java
+++ src/test/java/Armen/TestRegistration.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
-import javax.xml.ws.Action;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 public class TestRegistration {
