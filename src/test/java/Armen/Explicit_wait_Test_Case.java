0dee7b09163a0e3f51f146d840358f4be332e5ea
armharutyunyan23 <armharutyunyan.23@gmail.com>
2022-05-25 11:36:08 +0400

    Selenum

---
+++ src/test/java/Armen/Explicit_wait_Test_Case.java
@@ -0,0 +1,52 @@
+package Armen;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Explicit_wait_Test_Case {
+    public static  void  main(String[] args){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/");
+        driver.manage().window().maximize();
+        Actions actions=new Actions(driver);
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(20));
+       WebElement el = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]"));
+       actions.moveToElement(el).perform();
+       By More=By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[2]/span");
+       wait.until(ExpectedConditions.presenceOfElementLocated (More)).click();
+       By Quantity=By.xpath("//*[@id=\"quantity_wanted_p\"]/a[2]");
+       wait.until(ExpectedConditions.presenceOfElementLocated(Quantity)).click();
+       By Size=By.id("group_1");
+       wait.until(ExpectedConditions.presenceOfElementLocated(Size)).click();
+       By Size1=By.xpath("//*[@id=\"group_1\"]/option[2]");
+       wait.until(ExpectedConditions.presenceOfElementLocated(Size1)).click();
+       By Color =By.id("color_14");
+       wait.until(ExpectedConditions.presenceOfElementLocated(Color)).click();
+       By Add=By.xpath("//*[@id=\"add_to_cart\"]/button/span");
+       wait.until(ExpectedConditions.presenceOfElementLocated(Add)).click();
+        By checkout =By.xpath("//div/a[@title=\"Proceed to checkout\"]/span");
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkout)).click();
+        By checkout1 =By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span");
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkout1)).click();
+        By Email =By.xpath("//*[@id=\"email\"]");
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkout)).sendKeys("lj.28@gmail.com");
+        By pas =By.xpath("//*[@id=\"passwd\"]");
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkout)).sendKeys("Arm2003348");
+        By Sing_in =By.xpath("//*[@id=\"SubmitLogin\"]/span");
+        wait.until(ExpectedConditions.presenceOfElementLocated(checkout)).click();
+
+
+
+
+
+    }
+}
