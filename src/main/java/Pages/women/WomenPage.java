e8667935b29cf1c4e48869e1300a2baa3a3bbee2
ruzan <ruzanna5@live.com>
2022-07-05 21:16:16 +0400

    haha

--- src/main/java/Pages/women/WomenPage.java
+++ src/main/java/Pages/women/WomenPage.java
@@ -5,60 +5,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-    public class WomenPage extends BasePage {
-        private final BasePage basePage;
+public class WomenPage extends BasePage {
+    private final BasePage basePage;
 
-        public WomenPage(WebDriver driver) {
-            super(driver);
-            basePage = new BasePage(driver);
+    public WomenPage(WebDriver driver) {
+        super(driver);
+        basePage = new BasePage(driver);
+    }
 
-        }
+    @FindBy(xpath = "//a[@title=\"Women\"]")
+    public WebElement womenTitle;
 
-        @FindBy(xpath = "//a[@title=\"Women\"]")
-        public WebElement womenTitle;
+    @FindBy(id = "layered_category_8")
+    public WebElement dressCheckBox;
 
-        @FindBy(id = "uniform-layered_category_4")
-        public WebElement topsCategory;
+    @FindBy(id = "layered_id_attribute_group_1")
+    public WebElement sizeS;
 
-        @FindBy(id = "uniform-layered_category_8")
-        public WebElement dressesCategory;
+    @FindBy(id = "layered_id_attribute_group_2")
+    public WebElement sizeM;
 
-        @FindBy(id = "layered_id_attribute_group_1")
-        public WebElement sizeS;
+    public void womenTitle() {
+        basePage.waitElementTobeClickable(womenTitle);
+        basePage.clickOnelement(womenTitle);
+    }
 
-        @FindBy(id = "layered_id_attribute_group_2")
-        public WebElement sizeM;
+    public void DressCheckBox() {
+        basePage.clickOnelement(dressCheckBox);
+    }
 
-        @FindBy(id = "layered_id_attribute_group_3")
-        public WebElement sizeL;
+    public void sizeS() {
+        basePage.clickOnelement(sizeS);
+    }
 
-              
-        public void womenTitle(){
-                basePage.waitElementTobeClickable(womenTitle);
-                basePage.clickOnelement(womenTitle);
-                }
-
-        public void topsCategory(){
-                basePage.waitElementTobeClickable(topsCategory);
-                basePage.clickOnelement(topsCategory);
-                }
-        
-        public void dressesCategory(){
-                basePage.waitElementTobeClickable(dressesCategory);
-                basePage.clickOnelement(dressesCategory);
-                }
-
-        public void sizeS(){
-                basePage.waitElementTobeClickable(sizeS);
-                basePage.clickOnelement(sizeS);
-                }
-
-        public void sizeM(){
-                basePage.waitElementTobeClickable(sizeM);
-                basePage.clickOnelement(sizeM);
-                }
-
-        public void sizeL(){
-                basePage.waitElementTobeClickable(sizeL);
-                basePage.clickOnelement(sizeL);
-                }}
+    public void sizeM() {
+        basePage.waitElementTobeClickable(sizeM);
+        basePage.clickOnelement(sizeM);
+    }
+}
